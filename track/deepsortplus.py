319a4882e98483b0c618bef1791a9b79d9f6f949
johnnylord <johnnylord7410@gmail.com>
2021-07-11 13:24:22 +0800

    Add 3D kalman filter

---
+++ track/deepsortplus.py
@@ -0,0 +1,160 @@
+import scipy
+import numpy as np
+
+from .base import BaseTrack, TrackState
+from .motion.kalman3d import KalmanFilter3D
+from .utils.convert import tlbr_to_xyah, xyah_to_tlbr
+
+
+class ContextTrack(BaseTrack):
+    """Track object in DeepSORTPlus
+
+    Arguments:
+        box (ndarray): motion information (xmin, ymin, xmax, ymax, z, vx, vy)
+        feature (ndarray): ReID feature vector of 128 dimension
+        pool_size (ndarray): size of feature pool
+        max_depth (float): maximum depth of virtual space
+        n_levels (int): number of depth levels in virtual space
+    """
+    def __init__(self, box, feature, pool_size, max_depth, n_levels, **kwargs):
+        super().__init__(**kwargs)
+        self.pool_size = pool_size
+        # Motion Filter
+        self.kf = KalmanFilter3D(max_depth=max_depth, n_levels=n_levels)
+        # Initialize motion vector
+        x, y, a, h = tlbr_to_xyah(box[:4])
+        z, vx, vy = box[4:].tolist()
+        motion = np.array([ x, y, vx, vy, z, a, h ])
+        self.mean, self.covar = self.kf.initiate(motion)
+        # initilaize reid feature sets
+        self.feature_pool = [ feature ]
+
+    @property
+    def content(self):
+        if self.state == TrackState.TRACKED:
+            state = "tracked"
+        elif self.state == TrackState.LOST:
+            state = "lost"
+        elif self.state == TrackState.TENTATIVE:
+            state = "tentative"
+        else:
+            state = "dead"
+        # Content of track
+        mean = self.mean[:4]
+        xyah = [ mean[0], mean[1], mean[5], mean[6] ]
+        track = {
+            'id': self.id,
+            'state': state,
+            'box': xyah_to_tlbr(xyah),
+            'depth': self.mean[4],
+            'motion': [ self.mean[2], self.mean[3] ],
+            }
+        return track
+
+    def predict(self):
+        """Carrry track state from t-1 to t timestamp"""
+        mean, covar = self.kf.predict(self.mean, self.covar)
+        self.mean = mean
+        self.covar = covar
+        return self.mean, self.covar
+
+    def update(self, box):
+        """Use observation to calibrate track state at time t timestamp"""
+        # Convert observation format
+        x, y, a, h = tlbr_to_xyah(box[:4])
+        z, vx, vy = box[4:].tolist()
+        observation = np.array([ x, y, vx, vy, z, a, h ])
+        # Update state with observatio
+        mean, covar = self.kf.update(mean=self.mean,
+                                    covariance=self.covar,
+                                    observation=observation)
+        self.mean = mean
+        self.covar = covar
+        return self.mean, self.covar
+
+    def update_feature(self, feature):
+        """Update ReID feature pool"""
+        self.feature_pool.append(feature)
+        if len(self.feature_pool) >= self.pool_size:
+            self.feature_pool = self.feature_pool[1:]
+
+    def iou_dist(self, bboxes):
+        """Return iou distance vectors between track and bboxes
+
+        Args:
+            bboxes (np.ndarray): array of shape (N, 7)
+
+        Return:
+            A N dimensional iou distance vector
+
+        Note:
+            A bbox is (xmin, ymin, xmax, ymax, z, vx, vy)
+        """
+        xyah = [ self.mean[0], self.mean[1], self.mean[5], self.mean[6] ]
+        bbox = np.array([xyah_to_tlbr(xyah)])
+        bboxes = bboxes[:, :4]
+
+        x11, y11, x12, y12 = np.split(bbox, 4, axis=1)
+        x21, y21, x22, y22 = np.split(bboxes, 4, axis=1)
+
+        xA = np.maximum(x11, np.transpose(x21))
+        yA = np.maximum(y11, np.transpose(y21))
+        xB = np.minimum(x12, np.transpose(x22))
+        yB = np.minimum(y12, np.transpose(y22))
+
+        interArea = np.maximum((xB-xA+1), 0)*np.maximum((yB-yA+1), 0)
+        bbox1Area = (x12-x11+1)*(y12-y11+1)
+        bbox2Area = (x22-x21+1)*(y22-y21+1)
+
+        iou = interArea / (bbox1Area+np.transpose(bbox2Area)-interArea)
+        return (1 - iou).reshape(-1)
+
+    def cos_dist(self, features):
+        """Return cosine distance between features and feature pool of track
+
+        Args:
+            features (np.ndarray): array of shape (N, feature_dim)
+
+        Return:
+            A N dimensional cosine distance vecotr
+
+        Note:
+            Each feature vector is a unit vector
+        """
+        feature_pool = np.array(self.feature_pool)
+        cosines = np.dot(feature_pool, features.T)
+        return (1 - cosines).min(axis=0)
+
+    def square_maha_dist(self, bboxes):
+        """Return squared mahalanobis distance between track and bboxes
+
+        Args:
+            bboxes (np.ndarray): array of shape (N, 7)
+
+        Return:
+            A N dimensional distance vector
+
+        Note:
+            A bbox is (xmin, ymin, xmax, ymax, z, vx, vy)
+        """
+        # Convert bboxes format to state vector
+        observations = []
+        for bbox in bboxes:
+            x, y, a, h = tlbr_to_xyah(bbox[:4])
+            z, vx, vy = box[4:].tolist()
+            motion = np.array([ x, y, vx, vy, z, a, h ])
+            observations.append(motion)
+        observations = np.array(observations)
+
+        # Align dimension of state vector to observations
+        mean, covar = self.kf.project(self.mean, self.covar)
+
+        # Apply mahalonobis distance formula
+        cholesky_factor = np.linalg.cholesky(covar)
+        d = xyahs - mean
+        z = scipy.linalg.solve_triangular(cholesky_factor, d.T,
+                                        check_finite=False,
+                                        overwrite_b=True,
+                                        lower=True)
+        squared_maha = np.sum(z*z, axis=0)
+        return squared_maha
---
+++ track/motion/kalman3d.py
@@ -0,0 +1,224 @@
+import numpy as np
+import scipy.linalg
+
+
+__all__ = [ "KalmanFilter3D", "chi2inv95" ]
+
+"""
+Table for the 0.95 quantile of the chi-square distribution with N degrees of
+freedom (contains values for N=1, ..., 9). Taken from MATLAB/Octave's chi2inv
+function and used as Mahalanobis gating threshold.
+"""
+chi2inv95 = {
+    1: 3.8415,
+    2: 5.9915,
+    3: 7.8147,
+    4: 9.4877,
+    5: 11.070,
+    6: 12.592,
+    7: 14.067,
+    8: 15.507,
+    9: 16.919}
+
+
+class KalmanFilter3D:
+    """Kalman filter for modeling spatial state of track in image space
+
+    Here are the states kalman filter trying to maintain
+        np.ndarray([
+            x, y, z,            # 3D position
+            vx, vy,             # xy optical flow pixel offset
+            a, h,               # bounding box area
+            vz, ax, ay, va, vh  # velocities
+        ])
+
+    Target explanation:
+        x: bounding box center postion along x axis in image space
+        y: bounding box center postion along y axis in image space
+        z: bounding box center postion along z axis in pseudo depth space
+        vx: object center pixel offset velocity along x axis in image space
+        vy: object center pixel offset velocity along y axis in image space
+        a: aspect ratio of bounding box width over bounding box height
+        h: bounding box height
+        [va]*: respective velocities of target states
+
+    The motion model is a constant velocity model. The bounding box information
+    (x, y, z, vx, vy, a, h) are taken as direct observation.
+
+    Attributes:
+        _motion_mat (ndarray): a (12, 12) matrix for predicting the next state
+        _project_mat (ndarray): a (7, 12) matrix for projecting state vector from
+            state space to observation space.
+        _std_weight_pos (float): uncertainty of the (x, y, a, h)
+        _std_weight_vel (float): uncertainty of the (vx, vy, va, vh)
+        _std_weight_acc (float): uncertainty of the (ax, ay)
+        _std_weight_dep (float): uncertainty of the (z, vz)
+    """
+    def __init__(self, max_depth=5, n_levels=20):
+        self.max_depth = max_depth
+        self.n_levels = n_levels
+
+        # Dimension of prediction matrix: (12, 12)
+        self._motion_mat = np.array([
+            [ 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 ], # x
+            [ 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 ], # y
+            [ 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0 ], # z
+            [ 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0 ], # vx
+            [ 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0 ], # vy
+            [ 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0 ], # a
+            [ 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ], # h
+            # ==========================================
+            [ 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 ], # vz
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 ], # ax
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 ], # ay
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 ], # va
+            [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 ], # vh
+        ])
+
+        # Dimension of projection matrix: (7, 12)
+        self._project_mat = np.eye(7, 12)
+
+        # Motion and observation uncertainty are chosen relative to the current
+        # state estimation. These weights control the amount of uncertainty in
+        # the model. This is a bit hacky
+        self._std_weight_pos = 1. / 20
+        self._std_weight_vel = 1. / 320
+        self._std_weight_acc = 1. / 2048
+        self._std_weight_dep = max_depth / n_levels
+
+    def initiate(self, observation):
+        """Initialize state of kalman filter
+
+        Args:
+            observation (ndarray):
+                Bounding box information (x, y, z, vx, vy, a, h) with
+                center position (x, y, z),
+                xy optical flow (vx, vy),
+                aspect ratio (a),
+                and height (h).
+
+        Returns:
+            mean (ndarray): intialized state vector of shape (12,)
+            covariance (ndarray): initialized uncertainty matrix of shape (12x12)
+        """
+        # mean vector (state vector)
+        mean_state = observation
+        mean_veloc = np.array([0., 0., 0., 0., 0.])
+        mean = np.concatenate([mean_state, mean_veloc])
+
+        # covariance matrix (uncertainty matrix)
+        std = [
+            2 * self._std_weight_pos * observation[6],      # x ~= 50 pixel
+            2 * self._std_weight_pos * observation[6],      # y ~= 50 pixel
+            2 * self._std_weight_dep,                       # z
+            10 * self._std_weight_vel * observation[6],     # vx ~= 15 pixel
+            10 * self._std_weight_vel * observation[6],     # vy ~= 15 pixel
+            1e-2,                                           # a
+            2 * self._std_weight_pos * observation[6],      # h ~= 50 pixel
+            # =========================================================
+            self._std_weight_dep,                           # vz
+            10 * self._std_weight_acc * observation[6],     # ax ~= 2 pixel
+            10 * self._std_weight_acc * observation[6],     # ay ~= 2 pixel
+            1e-5,                                           # va
+            10 * self._std_weight_vel * observation[6],     # vh ~= 15 pixel
+            ]
+        covariance = np.diag(np.square(std))
+        return mean, covariance
+
+    def predict(self, mean, covariance):
+        """Predict the next state given the previous state (mean & covariance)
+
+        Args:
+            mean (ndarray): previous state vector of shape (12,)
+            covariance (ndarray): previous uncertainty matrix of shape (12, 12)
+
+        Returns:
+            mean (ndarray): predicted state vector of shape (12,)
+            covariance (ndarray): predicted uncertainty matrix of shape (12, 12)
+        """
+        # Noise for covariance matrix (noise from the "world", unknown factors)
+        std = [
+            self._std_weight_pos * mean[6],     # x ~= 50 pixel
+            self._std_weight_pos * mean[6],     # y ~= 50 pixel
+            self._std_weight_dep,               # z
+            self._std_weight_vel * mean[6],     # vx ~= 15 pixel
+            self._std_weight_vel * mean[6],     # vy ~= 15 pixel
+            1e-2,                               # a
+            self._std_weight_pos * mean[6],     # h ~= 50 pixel
+            # =========================================================
+            self._std_weight_dep,               # vz
+            self._std_weight_acc * mean[6],     # ax ~= 2 pixel
+            self._std_weight_acc * mean[6],     # ay ~= 2 pixel
+            1e-5,                               # va
+            self._std_weight_vel * mean[6],     # vh ~= 15 pixel
+            ]
+        motion_cov = np.diag(np.square(np.array(std)))
+
+        # Update mean vector and covariance matrix
+        mean = np.dot(self._motion_mat, mean)
+        covariance = np.linalg.multi_dot((
+            self._motion_mat, covariance, self._motion_mat.T)) + motion_cov
+
+        return mean, covariance
+
+    def project(self, mean, covariance):
+        """Project state vector and uncertainty matrix to the observation space
+
+        Args:
+            mean (ndarray): predicted state vector of shape (12,)
+            covariance (ndarray): predicted uncertainty matrix of shape (12, 12)
+
+        Returns:
+            mean (ndarray): projected state vector of shape (7,)
+            covariance (ndarray): projected uncertainty matrix of shape (7, 7)
+        """
+        # Noise for projected covariance matrix
+        std = [
+            self._std_weight_pos * mean[6],     # x ~= 50 pixel
+            self._std_weight_pos * mean[6],     # y ~= 50 pixel
+            self._std_weight_dep,               # z
+            self._std_weight_vel * mean[6],     # vx ~= 15 pixel
+            self._std_weight_vel * mean[6],     # vy ~= 15 pixel
+            1e-1,                               # a
+            self._std_weight_pos * mean[6],     # h ~= 50 pixel
+            ]
+        project_cov = np.diag(np.square(std))
+
+        # Projected mean and covariance matrix
+        mean = np.dot(self._project_mat, mean)
+        covariance = np.linalg.multi_dot((
+            self._project_mat, covariance, self._project_mat.T)) + project_cov
+
+        return mean, covariance
+
+    def update(self, mean, covariance, observation):
+        """Refine the predicted state with the observed data
+
+        Args:
+            mean (ndarray): predicted state vector of shape (12,)
+            covariacne (ndarray): predicted uncertainty matrix of shape (12, 12)
+            observation (ndarray): observed data of shape (7,)
+
+        Returns:
+            mean (ndarray): refined state vector of shape (12,)
+            covariacne (ndarray): refined uncertainty matrix of shape (12, 12)
+        """
+        # Project mean & covariance so that they are in the same space as observation
+        project_mean, project_covariance = self.project(mean, covariance)
+
+        # Calculate kalman gain
+        chol_factor, lower = scipy.linalg.cho_factor(
+                                    project_covariance,
+                                    lower=True,
+                                    check_finite=False)
+        kalman_gain = scipy.linalg.cho_solve(
+                                    (chol_factor, lower),
+                                    np.dot(covariance, self._project_mat.T).T,
+                                    check_finite=False).T
+
+        # Update mean and covariance with observation
+        mean = mean + np.dot(observation-project_mean, kalman_gain.T)
+        covariance = covariance - np.linalg.multi_dot((
+            kalman_gain, project_covariance, kalman_gain.T))
+
+        return mean, covariance
